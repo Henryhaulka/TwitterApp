4a2de327ba9dfe26a7acc0be04df6e93ca56909e
Henry Onu <onuhenry24@gmail.com>
2022-01-06 16:55:56 +0100

    Add image to user and cloudinary

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweeets
+  has_attached_file :avatar, storage: :cloudinary,
+                            path: ':id/:style/:filename',
+                            styles: { medium: '300x300>' },
+                            default_url: 'avatar.png'
+  validates_attachment_content_type :avatar, content_type: %r{\Aimage/.*\z}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_154319) do
+ActiveRecord::Schema.define(version: 2022_01_05_224140) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweet"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 2021_06_30_154319) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "username"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
