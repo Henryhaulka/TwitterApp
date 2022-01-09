ed55c0db45812a239a8e19b1bdd85ee77c8697a7
Henry Onu <onuhenry24@gmail.com>
2022-01-09 21:01:41 +0100

    Add association to user and followership

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweeets
+  has_many :followings, class_name: "Followership", foreign_key: "sender_id"
+  has_many :followers, class_name: "Followership", foreign_key: "receiver_id"
   has_attached_file :avatar, storage: :cloudinary,
                             path: ':id/:style/:filename',
                             styles: { medium: '300x300>' },
