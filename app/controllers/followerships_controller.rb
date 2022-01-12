19f28fe89c5e71adeae367eceac8361188585594
Henry Onu <onuhenry24@gmail.com>
2022-01-12 09:01:17 +0100

    Add logic for following and unfollowing

--- app/controllers/followerships_controller.rb
+++ app/controllers/followerships_controller.rb
@@ -1,2 +1,19 @@
 class FollowershipsController < ApplicationController
+    def create
+        user = params[:user]
+        follow = current_user.followings.build(receiver_id: user)
+        follow.save
+        @user = User.find(user)
+        redirect_to root_path, notice: "You are now following #{@user.name}"
+    end
+    
+    def destroy
+        user = params[:user]
+        following = current_user.followings.find_by(receiver_id: user)
+        following.destroy
+        @user = User.find(user)
+        redirect_to root_path, notice: "You have successfully unfollowed #{@user.name}"
+    end
+    
+    
 end
