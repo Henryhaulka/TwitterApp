c988195b519a4429a61a0647b3450347d93f8fea
Henry Onu <onuhenry24@gmail.com>
2022-01-14 16:56:22 +0100

    Add likes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :likes, only: [:create, :destroy, :index]
   resources :followerships, only: [:create, :destroy, :index]
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :tweeets
