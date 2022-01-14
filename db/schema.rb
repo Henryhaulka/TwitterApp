d1411e8701ee0ed36c0d043601900ce0fe120178
Henry Onu <onuhenry24@gmail.com>
2022-01-14 09:59:37 +0100

    Add image to tweet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_09_132840) do
+ActiveRecord::Schema.define(version: 2022_01_12_153208) do
 
   create_table "followerships", force: :cascade do |t|
     t.integer "sender_id"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2022_01_09_132840) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "image"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
