44b572c6bc40f789e3e5073521b87cf3cd8a2767
Henry Onu <onuhenry24@gmail.com>
2022-01-20 09:11:52 +0100

    Add create action to commentcontroller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+    def create
+        tweet = Tweeet.find(params[:tweeet_id])
+        commenting = tweet.comments.build(comment_params)
+        commenting.user = current_user
+        commenting.save
+        redirect_to tweeet_path(tweet), notice: 'successfully commented'
+    end
+
+    private
+    def comment_params
+        params.require(:comment).permit(:body)
+    end
+    
+    
 end
