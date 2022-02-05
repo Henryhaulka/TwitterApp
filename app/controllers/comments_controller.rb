7a83ac0a16108c4157f25a45a9f40d1736d48c62
Henry Onu <onuhenry24@gmail.com>
2022-02-05 14:22:49 +0100

    Add destroy action to controller and view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,16 @@ class CommentsController < ApplicationController
         redirect_to tweeet_path(tweet), notice: 'successfully commented'
     end
 
+    def destroy
+        tweet_comment = params[:twit]
+        delete_comment = current_user.comments.find_by(tweeet_id: tweet_comment)
+        delete_comment.destroy
+        tweet = Tweeet.find(params[:twit])
+        redirect_to tweeet_path(tweet), alert: 'Comment deleted successfully'
+
+    end
+    
+
     private
     def comment_params
         params.require(:comment).permit(:body)
