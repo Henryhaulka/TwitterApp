5628ea045034caae55d326082cff734b9e3e0665
Henry Onu <onuhenry24@gmail.com>
2022-02-07 09:22:51 +0100

    Add resources for subcomment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  
   resources :likes, only: [:create, :destroy, :index]
   resources :followerships, only: [:create, :destroy, :index]
   devise_for :users, :controllers => {registrations: 'registrations'}
   resources :tweeets do
-    resources :comments
+    resources :comments do
+      resources :sub_comments
+    end
   end
   resources :users
    
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_101459) do
+ActiveRecord::Schema.define(version: 2022_02_05_213536) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 2022_01_18_101459) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["tweeet_id"], name: "index_likes_on_tweeet_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "sub_comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "comment_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
+    t.index ["comment_id"], name: "index_sub_comments_on_comment_id"
+    t.index ["user_id"], name: "index_sub_comments_on_user_id"
   end
 
   create_table "tweeets", force: :cascade do |t|
@@ -73,4 +83,6 @@ ActiveRecord::Schema.define(version: 2022_01_18_101459) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "tweeets"
   add_foreign_key "likes", "users"
+  add_foreign_key "sub_comments", "comments"
+  add_foreign_key "sub_comments", "users"
 end
