ffed228c64f38eac283d23a016c24203ccb0bc8d
Henry Onu <onuhenry24@gmail.com>
2022-02-08 14:02:00 +0100

    Add logic for sub_comment in sub_comment controlla

--- app/controllers/sub_comments_controller.rb
+++ app/controllers/sub_comments_controller.rb
@@ -1,2 +1,17 @@
 class SubCommentsController < ApplicationController
+    def create
+        tweet = Tweeet.find(params[:tweeet_id])
+        comm = Comment.find_by(tweeet_id: tweet)
+        sub = SubComment.create(sub_params)
+        sub.user = current_user
+        sub.comment = comm
+        sub.save
+        redirect_to tweeet_path(tweet), notice: "You have replied a comment by #{comm.user.username}"
+    end
+    
+
+    private
+    def sub_params
+        params.require(:sub_comment).permit(:body)
+    end
 end
