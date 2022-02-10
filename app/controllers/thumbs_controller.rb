8a64cff48d4593e2f44f6ea4977fda9893f811d6
Henry Onu <onuhenry24@gmail.com>
2022-02-10 10:32:59 +0100

    Add create method to thumb controller

--- app/controllers/thumbs_controller.rb
+++ app/controllers/thumbs_controller.rb
@@ -1,2 +1,15 @@
 class ThumbsController < ApplicationController
+    def create
+       tweet = params[:tweet]
+       comment = params[:com]
+       thumb = current_user.thumbs.create!(tweeet_id: tweet, comment_id: comment)
+       twit = Tweeet.find(params[:tweet])
+       redirect_to tweeet_path(twit), notice: 'comment liked' if thumb.save
+    end
+
+    def destroy
+        thumb = current_user.thumbs.find_by()
+    end
+    
+
 end
