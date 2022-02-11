68577c2e8c03aba9858f17a748bfe02c89a861d5
Henry Onu <onuhenry24@gmail.com>
2022-02-11 09:59:53 +0100

    Add subcomment test

---
+++ spec/models/sub_comment_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+RSpec.describe SubComment, type: :model do
+    describe "a SubComment Association" do
+        it {should belong_to(:user)}
+        it {should belong_to(:comment)}
+        it {should respond_to(:body)}
+    end
+end
