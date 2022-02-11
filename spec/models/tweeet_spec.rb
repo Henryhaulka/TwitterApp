28a7fe4193cf6f4331d9c9b0d95a525684a2fa01
Henry Onu <onuhenry24@gmail.com>
2022-02-11 09:47:00 +0100

    Add tweeet test

---
+++ spec/models/tweeet_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+RSpec.describe Tweeet, type: :model do
+    describe "a Tweeet Association" do
+        it {should belong_to(:user)}
+        it {should have_many(:likes)}
+        it {should have_many(:comments)}
+        it {should have_many(:thumbs)}
+        it {should have_many(:unthumbs)}
+        it {should respond_to(:image)}
+    end
+end
