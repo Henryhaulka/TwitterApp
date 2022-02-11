0a568986635dac0d1f55c4a0ac9a773e32bfc3c0
Henry Onu <onuhenry24@gmail.com>
2022-02-11 09:46:46 +0100

    Add user test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe User, type: :model do
+    describe "a User Association" do
+        it {should have_many(:tweeets)}
+        it {should have_many(:likes)}
+        it {should have_many(:comments)}
+        it {should have_many(:thumbs)}
+        it {should have_many(:unthumbs)}
+        it {should respond_to(:avatar)}
+    end
+end
