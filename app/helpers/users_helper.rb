0f0ab78853c26638ba7c450cef054bfc8a14dd0b
Henry Onu <onuhenry24@gmail.com>
2022-02-15 10:11:33 +0100

    Add user format helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+    def user_format(user)
+        link = ''
+        if  current_user.followings.where(receiver_id: user.id).present? 
+         link  << link_to('Unfollow',followership_path(user: user), method: :delete, data: { confirm: "Are you sure you want to unfollow #{user.name}"}, class: 'btn btn-danger w-50' )
+        elsif current_user == user
+         link = content_tag :span, ' Current User',   class: 'btn btn-success w-50'
+        else
+         link  << link_to('Follow',followerships_path(user: user), method: :post, class: 'btn btn-primary w-50' )
+        end  
+        link.html_safe
+    end 
 end
+
+
+ 
